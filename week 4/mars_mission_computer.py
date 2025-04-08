c7fdc58ad7768ecce03b43447ae69b61b606581f
yoona0504 <102075041+yoona0504@users.noreply.github.com>
2025-04-08 19:55:51 +0900

    mars_mission_computer.py

--- week 4/mars_mission_computer.py
+++ week 4/mars_mission_computer.py
@@ -1,28 +1,11 @@
 import time
 import json
+from datetime import datetime, timedelta
+from dummy_sensor import DummySensor
 
-class DummySensor:
-    def get_internal_temperature(self):
-        return 21.5
-
-    def get_external_temperature(self):
-        return -63.0
-
-    def get_internal_humidity(self):
-        return 40.0
-
-    def get_external_illuminance(self):
-        return 1000.0
-
-    def get_internal_co2(self):
-        return 0.03
-
-    def get_internal_oxygen(self):
-        return 20.8
 
 class MissionComputer:
     def __init__(self):
-        # 화성 기지 환경값 저장용 딕셔너리
         self.env_values = {
             'mars_base_internal_temperature': 0.0,
             'mars_base_external_temperature': 0.0,
@@ -31,52 +14,68 @@ class MissionComputer:
             'mars_base_internal_co2': 0.0,
             'mars_base_internal_oxygen': 0.0
         }
-
-        # 최근 5분 간 데이터 기록용 (최대 60개씩 저장)
-        self.history = {key: [] for key in self.env_values}
-
-        # 더미 센서 인스턴스 생성
         self.ds = DummySensor()
+        self.log_history = []
 
     def get_sensor_data(self):
-        start_time = time.time()
-        print('Ctrl+C 를 눌러 출력을 멈추세요.')
+        start_time = datetime.now()
+        print('Mission Computer started. Press "q" to stop.')
 
-        try:
-            while True:
-                # 센서로부터 값을 읽어 env_values에 저장
-                self.env_values['mars_base_internal_temperature'] = self.ds.get_internal_temperature()
-                self.env_values['mars_base_external_temperature'] = self.ds.get_external_temperature()
-                self.env_values['mars_base_internal_humidity'] = self.ds.get_internal_humidity()
-                self.env_values['mars_base_external_illuminance'] = self.ds.get_external_illuminance()
-                self.env_values['mars_base_internal_co2'] = self.ds.get_internal_co2()
-                self.env_values['mars_base_internal_oxygen'] = self.ds.get_internal_oxygen()
+        while True:
+            if self.check_for_exit():
+                print('System stopped…')
+                break
 
-                # 각 항목별로 history에 추가 60개 초과 시 가장 오래된 항목 제거
-                for key in self.env_values:
-                    self.history[key].append(self.env_values[key])
-                    if len(self.history[key]) > 60:
-                        self.history[key].pop(0)
+            # 센서 데이터 수집
+            self.ds.set_env()
+            new_data = self.ds.get_env()
+            self.env_values = new_data
 
-                # 현재 센서 데이터를 JSON 형식으로 출력
-                print(json.dumps(self.env_values, indent=2))
+            # JSON 출력
+            print(json.dumps(self.env_values, indent=4))
 
-                # 경과 시간이 5분(=300초)에 가까우면 평균값 출력
-                elapsed = time.time() - start_time
-                if int(elapsed) % 300 < 5:
-                    print('[5분 평균값]')
-                    avg_values = {}
-                    for key in self.history:
-                        avg = sum(self.history[key]) / len(self.history[key])
-                        avg_values[key] = round(avg, 2)
-                    print(json.dumps(avg_values, indent=2))
+            # 로그 기록
+            timestamp = datetime.now()
+            self.log_history.append((timestamp, new_data))
 
-                # 5초 대기 후 반복
-                time.sleep(5)
+            # 5분 평균 출력
+            if (timestamp - start_time).seconds >= 60:
+                self.print_5min_average()
+                start_time = timestamp
 
-        except KeyboardInterrupt:
-            # Ctrl+C 입력 시 종료 메시지 출력
-            print('System stopped…')
+            time.sleep(5)
+
+    def check_for_exit(self):
+        import msvcrt
+        if msvcrt.kbhit():
+            key = msvcrt.getch().decode('utf-8').lower()
+            if key == 'q':
+                return True
+        return False
+
+    def print_5min_average(self):
+        now = datetime.now()
+        five_minutes_ago = now - timedelta(minutes=5)
+
+        recent_data = [
+            data for ts, data in self.log_history
+            if five_minutes_ago <= ts <= now
+        ]
+
+        if not recent_data:
+            print('No data available for 5-minute average.')
+            return
+
+        average_data = {}
+        keys = self.env_values.keys()
+
+        for key in keys:
+            values = [d[key] for d in recent_data]
+            average_data[key] = sum(values) / len(values)
+
+        print('---- 5 Minute Average ----')
+        print(json.dumps(average_data, indent=4))
+
 
 if __name__ == '__main__':
     RunComputer = MissionComputer()
